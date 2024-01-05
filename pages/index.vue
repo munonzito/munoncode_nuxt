ef853be04b9c28c7c8079caed0cf59e3013f425e
Martin Muñoz <martin.munoz.a@gmail.com>
2024-01-05 13:34:04 -0300

    images

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="main flex flex-col gap-10 px-5 py-10 lg:p-10 lg:flex-row">
         <div class="flex gap-5 lg:flex-col lg:mr-20">
-            <img class="w-32 h-32 rounded-full" src="https://images-ext-2.discordapp.net/external/_g3yTiOuyF-We75avCfForGUH97hiLg0Ngh7TxDf8fU/%3Fstp%3Ddst-jpg_s320x320%26_nc_ht%3Dinstagram.fscl1-1.fna.fbcdn.net%26_nc_cat%3D103%26_nc_ohc%3D411u9t_ds3YAX9hpHpd%26edm%3DAOQ1c0wBAAAA%26ccb%3D7-5%26oh%3D00_AfD7-WGuXfSX5iewSHqDi13BgHvhsSkiOMHZdEsmNpOs6w%26oe%3D6584EBAF%26_nc_sid%3D8b3546/https/instagram.fscl1-1.fna.fbcdn.net/v/t51.2885-19/402904674_237572645808243_2763874709548207554_n.jpg?format=webp&width=400&height=400" alt="">
+            <img class="w-32 h-32 rounded-full" src="assets/public/img/profile-pic.png" alt="">
             <div class="flex flex-col space-y-2">
                 <h1 class="text-2xl font-bold">Martín Muñoz</h1>
                 <h2 class="text-xl font-bold">@munoncode</h2>
@@ -36,6 +36,12 @@
          description: 'Remueve el Fondo de Tus Fotos Usando Inteligencia Artificial',
          image: 'https://play-lh.googleusercontent.com/wJEqeRZGZds8tVZKQq-azZ1sp7IjYgF92zXVKYD1vy1LrV6xz4KVriABCjdjS8jQyA=w240-h480-rw',
          url: 'https://play.google.com/store/apps/details?id=com.munoncode.aimage'
+        },
+        {
+         title: 'Threads AI',
+         description: 'Escribe Posts Para Threads Usando La IA de ChatGPT ',
+         image: 'https://play-lh.googleusercontent.com/jOSwNh-LBNEp2pQHNBoc9FLKdrYADgm3BgWN3CgxbnEzrJVMqb3l0CE_Nc05u9ZJb3o=w240-h480-rw',
+         url: 'https://play.google.com/store/apps/details?id=com.munoncode.threadsai'
         },
      {
          title: 'Gigachad Esquiva Balas',
