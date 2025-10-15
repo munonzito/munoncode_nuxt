1e24924adf50bfd9894824666da48e80d13d05ca
Martín Muñoz <martin.munoz.a@gmail.com>
2025-10-14 22:55:01 -0300

    date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2025-10-14',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css',
-'@fortawesome/fontawesome-svg-core/styles.css'
-],
+    '@fortawesome/fontawesome-svg-core/styles.css'
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
